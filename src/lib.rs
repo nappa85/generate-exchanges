69ce317c0bf9185035ae2f016e28f02ccaa4df1c
Marco Napetti <nappa85@therocktrading.com>
2021-06-29 17:06:29 +0200

    add indirect methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use std::sync::RwLock;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{Expr, ExprArray, ExprPath, PathArguments, parse_macro_input, punctuated::Punctuated, ItemMod, Item, PathSegment, Path, token::Bracket};
+use syn::{Arm, AttrStyle, Attribute, Expr, ExprArray, ExprMatch, Field, Fields, FieldsUnnamed, GenericParam, Generics, Item, ItemEnum, ItemMod, Lifetime, LifetimeDef, Pat, PatLit, Path, PathArguments, PathSegment, Type, Variant, VisPublic, Visibility, parse_macro_input, punctuated::Punctuated, token::{Brace, Bracket, Colon, Comma, Enum, FatArrow, Gt, Lt, Match, Paren, Pound, Pub}};
+
+use once_cell::sync::Lazy;
+
+static MAP: Lazy<RwLock<Vec<String>>> = Lazy::new(|| RwLock::new(Vec::new()));
 
 #[proc_macro_attribute]
 pub fn generate_exchanges(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -12,19 +18,8 @@ pub fn generate_exchanges(args: TokenStream, input: TokenStream) -> TokenStream 
     if let Some((_, contents)) = &item_mod.content {
         for content in contents {
             if let Item::Const(item) = content {
-                let mut segments = Punctuated::new();
-                segments.push(PathSegment {
-                    ident: item.ident.clone(),
-                    arguments: PathArguments::None,
-                });
-                v.push(Expr::Path(ExprPath {
-                    attrs: Vec::new(),
-                    qself: None,
-                    path: Path {
-                        leading_colon: None,
-                        segments
-                    }
-                }));
+                let i = &item.ident;
+                v.push(Expr::Verbatim(quote! { #i }));
             }
         }
     }
@@ -35,6 +30,10 @@ pub fn generate_exchanges(args: TokenStream, input: TokenStream) -> TokenStream 
         elems: v
     };
     let name = &item_mod.ident;
+    {
+        let mut lock = MAP.write().unwrap();
+        lock.push(name.to_string());
+    }
     return quote! {
         pub mod #name {
             #item_mod
@@ -93,4 +92,241 @@ pub fn generate_exchanges(args: TokenStream, input: TokenStream) -> TokenStream 
         }
     }
     .into();
+}
+
+#[proc_macro]
+pub fn generate_map(_: TokenStream) -> TokenStream {
+    // create a GenericMoney enum
+    let mut gm_enum = ItemEnum {
+        attrs: vec![Attribute {
+            pound_token: Pound::default(),
+            style: AttrStyle::Outer,
+            bracket_token: Bracket::default(),
+            path: Path {
+                leading_colon: None,
+                segments: {
+                    let mut s = Punctuated::new();
+                    s.push(PathSegment {
+                        ident: Ident::new("derive", Span::call_site()),
+                        arguments: PathArguments::None,
+                    });
+                    s
+                }
+            },
+            tokens: quote! { (Debug, PartialEq) },
+        }],
+        vis: Visibility::Public(VisPublic {
+            pub_token: Pub::default(),
+        }),
+        enum_token: Enum::default(),
+        ident: Ident::new("GenericMoney", Span::call_site()),
+        generics: Generics {
+            lt_token: Some(Lt::default()),
+            params: {
+                let mut p = Punctuated::new();
+                p.push(GenericParam::Lifetime(LifetimeDef {
+                    attrs: Vec::new(),
+                    lifetime: Lifetime {
+                        apostrophe: Span::call_site(),
+                        ident: Ident::new("a", Span::call_site()),
+                    },
+                    colon_token: None,
+                    bounds: Punctuated::new(),
+                }));
+                p
+            },
+            gt_token: Some(Gt::default()),
+            where_clause: None,
+        },
+        brace_token: Brace::default(),
+        variants: Punctuated::new(),
+    };
+    // create a GenericCurrency enum
+    let mut gc_enum = ItemEnum {
+        attrs: vec![Attribute {
+            pound_token: Pound::default(),
+            style: AttrStyle::Outer,
+            bracket_token: Bracket::default(),
+            path: Path {
+                leading_colon: None,
+                segments: {
+                    let mut s = Punctuated::new();
+                    s.push(PathSegment {
+                        ident: Ident::new("derive", Span::call_site()),
+                        arguments: PathArguments::None,
+                    });
+                    s
+                }
+            },
+            tokens: quote! { (Debug, PartialEq) },
+        }],
+        vis: Visibility::Public(VisPublic {
+            pub_token: Pub::default(),
+        }),
+        enum_token: Enum::default(),
+        ident: Ident::new("GenericCurrency", Span::call_site()),
+        generics: Generics {
+            lt_token: Some(Lt::default()),
+            params: {
+                let mut p = Punctuated::new();
+                p.push(GenericParam::Lifetime(LifetimeDef {
+                    attrs: Vec::new(),
+                    lifetime: Lifetime {
+                        apostrophe: Span::call_site(),
+                        ident: Ident::new("a", Span::call_site()),
+                    },
+                    colon_token: None,
+                    bounds: Punctuated::new(),
+                }));
+                p
+            },
+            gt_token: Some(Gt::default()),
+            where_clause: None,
+        },
+        brace_token: Brace::default(),
+        variants: Punctuated::new(),
+    };
+    // create a match expression
+    let mut load_match = ExprMatch {
+        attrs: Vec::new(),
+        match_token: Match::default(),
+        expr: Box::new(Expr::Verbatim(quote!{ family })),
+        brace_token: Brace::default(),
+        arms: Vec::new(),
+    };
+    // create a match expression
+    let mut save_match = ExprMatch {
+        attrs: Vec::new(),
+        match_token: Match::default(),
+        expr: Box::new(Expr::Verbatim(quote!{ value })),
+        brace_token: Brace::default(),
+        arms: Vec::new(),
+    };
+    // create a match expression
+    let mut convert_match = ExprMatch {
+        attrs: Vec::new(),
+        match_token: Match::default(),
+        expr: Box::new(Expr::Verbatim(quote!{ (from, to) })),
+        brace_token: Brace::default(),
+        arms: Vec::new(),
+    };
+    // add an arm for every module
+    for m in MAP.read().unwrap().iter() {
+        let i = Ident::new(m, Span::call_site());
+        gm_enum.variants.push(Variant {
+            attrs: Vec::new(),
+            ident: i.clone(),
+            fields: Fields::Unnamed(FieldsUnnamed {
+                paren_token: Paren::default(),
+                unnamed: {
+                    let mut u = Punctuated::new();
+                    u.push(Field {
+                        attrs: Vec::new(),
+                        vis: Visibility::Inherited,
+                        ident: None,
+                        colon_token: Some(Colon::default()),
+                        ty: Type::Verbatim(quote! { Money<'a, #i::Currency> }),
+                    });
+                    u
+                },
+            }),
+            discriminant: None,
+        });
+        gc_enum.variants.push(Variant {
+            attrs: Vec::new(),
+            ident: i.clone(),
+            fields: Fields::Unnamed(FieldsUnnamed {
+                paren_token: Paren::default(),
+                unnamed: {
+                    let mut u = Punctuated::new();
+                    u.push(Field {
+                        attrs: Vec::new(),
+                        vis: Visibility::Inherited,
+                        ident: None,
+                        colon_token: Some(Colon::default()),
+                        ty: Type::Verbatim(quote! { &'a #i::Currency }),
+                    });
+                    u
+                },
+            }),
+            discriminant: None,
+        });
+        load_match.arms.push(Arm {
+            attrs: Vec::new(),
+            pat: Pat::Lit(PatLit {
+                attrs: Vec::new(),
+                expr: Box::new(Expr::Verbatim(quote!{ #m })),
+            }),
+            guard: None,
+            fat_arrow_token: FatArrow::default(),
+            body: Box::new(Expr::Verbatim(quote!{ Some(GenericMoney::#i(#i::load(qty))) })),
+            comma: Some(Comma::default()),
+        });
+        save_match.arms.push(Arm {
+            attrs: Vec::new(),
+            pat: Pat::Lit(PatLit {
+                attrs: Vec::new(),
+                expr: Box::new(Expr::Verbatim(quote!{ GenericMoney::#i(m) })),
+            }),
+            guard: None,
+            fat_arrow_token: FatArrow::default(),
+            body: Box::new(Expr::Verbatim(quote!{ #i::save(m) })),
+            comma: Some(Comma::default()),
+        });
+        convert_match.arms.push(Arm {
+            attrs: Vec::new(),
+            pat: Pat::Lit(PatLit {
+                attrs: Vec::new(),
+                expr: Box::new(Expr::Verbatim(quote!{ (GenericMoney::#i(m), GenericCurrency::#i(c)) })),
+            }),
+            guard: None,
+            fat_arrow_token: FatArrow::default(),
+            body: Box::new(Expr::Verbatim(quote!{ Ok(GenericMoney::#i(#i::convert(m, c)?)) })),
+            comma: Some(Comma::default()),
+        });
+    }
+    // create a fallback arm
+    load_match.arms.push(Arm {
+        attrs: Vec::new(),
+        pat: Pat::Lit(PatLit {
+            attrs: Vec::new(),
+            expr: Box::new(Expr::Verbatim(quote!{ _ })),
+        }),
+        guard: None,
+        fat_arrow_token: FatArrow::default(),
+        body: Box::new(Expr::Verbatim(quote!{ None })),
+        comma: Some(Comma::default()),
+    });
+    // create a fallback arm
+    convert_match.arms.push(Arm {
+        attrs: Vec::new(),
+        pat: Pat::Lit(PatLit {
+            attrs: Vec::new(),
+            expr: Box::new(Expr::Verbatim(quote!{ _ })),
+        }),
+        guard: None,
+        fat_arrow_token: FatArrow::default(),
+        body: Box::new(Expr::Verbatim(quote!{ Err(rusty_money::MoneyError::InvalidCurrency) })),
+        comma: Some(Comma::default()),
+    });
+    // output result
+    return quote! {
+        use rusty_money::MoneyError;
+
+        #gc_enum
+
+        #gm_enum
+
+        pub fn load<'a>(family: &str, qty: Decimal) -> Option<GenericMoney<'a>> {
+            #load_match
+        }
+
+        pub fn save<'a>(value: GenericMoney<'a>) -> Result<Decimal, MoneyError> {
+            #save_match
+        }
+
+        pub fn convert<'a>(from: GenericMoney<'a>, to: GenericCurrency<'a>) -> Result<GenericMoney<'a>, MoneyError> {
+            #convert_match
+        }
+    }.into();
 }
--- tests/it_works.rs
+++ tests/it_works.rs
@@ -1,5 +1,5 @@
 
-use generate_exchanges::generate_exchanges;
+use generate_exchanges::{generate_exchanges, generate_map};
 
 use rust_decimal::Decimal;
 
@@ -44,15 +44,75 @@ define_currency_set!(
             symbol: "satoshi",
             symbol_first: true,
         }
+    },
+    #[generate_exchanges(GWEI)]
+    ether {
+        ETH: {
+            code: "ETH",
+            exponent: 18,
+            locale: Locale::EnUs,
+            minor_units: 1_000_000_000_000_000_000,
+            name: "ETH",
+            symbol: "ETH",
+            symbol_first: false,
+        },
+        FINNEY: {
+            code: "finney",
+            exponent: 15,
+            locale: Locale::EnUs,
+            minor_units: 1_000_000_000_000_000,
+            name: "finney",
+            symbol: "finney",
+            symbol_first: false,
+        },
+        SZABO: {
+            code: "szabo",
+            exponent: 12,
+            locale: Locale::EnUs,
+            minor_units: 1_000_000_000_000,
+            name: "szabo",
+            symbol: "szabo",
+            symbol_first: false,
+        },
+        GWEI: {
+            code: "gwei",
+            exponent: 9,
+            locale: Locale::EnUs,
+            minor_units: 1_000_000_000,
+            name: "gwei",
+            symbol: "gwei",
+            symbol_first: false,
+        },
+        WEI: {
+            code: "wei",
+            exponent: 0,
+            locale: Locale::EnUs,
+            minor_units: 1,
+            name: "wei",
+            symbol: "wei",
+            symbol_first: false,
+        }
     }
 );
 
+generate_map!();
+
 #[test]
-fn it_works() {
+fn direct() {
     let value = Decimal::new(1, 0);
     let value1 = btc::load(value);
     assert_eq!(value1, Money::from_major(1, btc::BTC));
     let value2 = btc::convert(value1, btc::SATOSHI).unwrap();
     assert_eq!(value2, Money::from_major(100000000, btc::SATOSHI));
     assert_eq!(value, btc::save(value2).unwrap());
+}
+
+#[test]
+fn indirect() {
+    let value = Decimal::new(1, 0);
+    let value1 = load("btc", value).unwrap();
+    assert_eq!(value1, GenericMoney::btc(Money::from_major(1, btc::BTC)));
+    let value2 = convert(value1, GenericCurrency::btc(btc::SATOSHI)).unwrap();
+    assert_eq!(value2, GenericMoney::btc(Money::from_major(100000000, btc::SATOSHI)));
+    assert_eq!(value, save(value2).unwrap());
 }
